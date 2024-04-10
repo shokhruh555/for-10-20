1402b29b4de34818e506dd9166e0ebd0d67a523d
Shokhruh555 <Shohruhismatov54@gmail.com>
2024-04-10 13:45:13 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,119 @@
+// FOR-11 
+ 
+ 
+// let n = 1 ; 
+// let total = 0; 
+// for (let i = n; i <= 2 * n; i++) { 
+//      total += i ** 2; 
+// } 
+// console.log(total); 
+ 
+ 
+ 
+// FOR-12 
+ 
+// let n = 3; 
+// let result = 1.1; 
+// for (let i = 1.1; i <= n; i += 1) { 
+//     result *= i; 
+// } 
+// console.log(result); 
+ 
+ 
+// FOR-13 
+ 
+// let n = 3 , a = 0.0 , b = 1 , c = 1.1; 
+ 
+// for (let i = 1; i <= n; i++) { 
+//     a += b * c; 
+//     c += 0.1; 
+//     b *= -1; 
+// } 
+ 
+// console.log(a); 
+ 
+ 
+ 
+// FOR-14 
+ 
+ 
+// let n = 7 , count = 1; 
+ 
+ 
+// for (let i = 3; i <= n; i+=2) { 
+//     console.log(`${count} + ${i}`); 
+//     count += i 
+// } 
+ 
+// console.log(Math.pow(count, 2)); 
+ 
+ 
+// FOR-15 
+ 
+ 
+// let a = 2 , b = 3 ; 
+// for (let i = 1; i <= 1; i++) { 
+//     a **= b; 
+// } 
+// console.log(a);
+// FOR-16 
+ 
+ 
+// let a = 3, b = 3 ; 
+ 
+// for (let i = 0; i <= b; i++) { 
+//    c = a**i 
+//    console.log(c) 
+// } 
+ 
+ 
+ 
+// FOR-17 
+ 
+ 
+ 
+// let a = 5, b = 2 , c = 0; 
+ 
+// for (let i = 0; i <= b; i++) { 
+//    c += Math.pow(a,i) 
+//    console.log("c="+c); 
+// } 
+// console.log(c); 
+ 
+ 
+ 
+ 
+// FOR-18 
+ 
+// let a = 2, b = 2, c = 0; 
+ 
+// for (let i = 0; i <= b; i++) { 
+//    c += a ** i ; 
+//    console.log("c="+c); 
+//    a *= -1; 
+// } 
+// console.log(c); 
+ 
+// FOR-19 
+ 
+// let n = 5 , count = 1; 
+ 
+// for (let i = 1; i <= n; i++) { 
+//    count *= i 
+//    console.log(count); 
+// } 
+ 
+ 
+ 
+// FOR-20 
+ 
+// let n = 4 , count = 0; 
+ 
+// for (let i = 1; i <= n; i++) { 
+//    let x = 1;   
+//    for (let j = 1 ; j <= i; j++) { 
+//       x*=j 
+//    } 
+//    count += x 
+// } 
+// console.log(count);
